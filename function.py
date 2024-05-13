9746fb4ad99e85b98b9ac65ab0a07242ad599f08
kunwardigvijay <kunwardigvijay1@gmail.com>
2024-05-13 19:46:53 +0530

    README file

---
+++ function.py
@@ -0,0 +1,67 @@
+# def avg (a, b, c):
+#     sum = (a + b + c) / 3
+#     print(sum)
+#     return sum
+# #new code    
+# avg (3,5,7)   
+
+# def converter(used_value):
+#     inr_val = used_value * 83
+#     print(inr_val)
+    
+# 2nd problem
+# def add(num1, num2):
+#     return num1 + num1     
+# print(add(3,3))       
+  
+# 3rd problem          
+# def multiply(p1, p2):
+#     return p1 * p2
+
+# print(multiply(3,4))
+# print(multiply("r",4))
+# print(multiply(5, "y"))
+
+# 4th problem  
+# import math
+# def circle_stata(radius):
+#     area =math.pi * radius ** 2
+#     circumferenca = 2 * math.pi  * radius
+#     return area ,circumferenca
+    
+# print(circle_stata(2))
+
+# 5th problem
+# def greet(name = "user"):
+#     return "Hello, " + name + " !"
+
+# print(greet("kd"))   
+
+# 6th problem
+# cube = lambda x: x ** 3  # lamba is key word it replace funcation
+
+# print(cube(4))
+
+# # 7th problem
+# def sum_all(*args):  # args = argument
+#     # print(args)
+#     # for i in args:  #see diff reaction
+#     #     print(i * 2)
+#     return sum(args) # sum is inbuilt key
+
+# print(sum_all(1, 2, 3))
+        
+# 8th problem
+# def print_kwargs(**kwargs):
+#     for key, value in kwargs.items():
+#         print(f"{key}:{value}")
+    
+# print_kwargs(name="shaktiman", power="lazer")    
+# print_kwargs(name="shaktiman")  
+# print_kwargs(name="shaktiman", power="lazer",enemy="jackaal")  
+
+# 9th problem
+ 
+
+
+
---
+++ oops.py
@@ -0,0 +1,15 @@
+# # basic problem
+class car:
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
+        
+my_car = car ("Toyota", "Corolla")
+print(my_car.brand)
+print(my_car.model)
+
+my_new_car = car ("Tata", "Safari")
+print(my_new_car.model) 
+print(my_new_car.brand) 
+      
+
